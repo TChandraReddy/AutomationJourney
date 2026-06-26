e6b9912b6bfb873e0e3991461206b57387512b2e
Chandra <tadi_chandra@yahoo.com>
2026-06-26 15:04:10 +0530

    Add BranchPractice demo on feature branch

---
+++ BranchPractice.java
@@ -0,0 +1,6 @@
+public class BranchPractice {
+    public static void main(String[] args) {
+        System.out.println("This code was written on a feature branch!");
+        System.out.println("It will be merged into main after review.");
+    }
+}
